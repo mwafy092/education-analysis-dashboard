b58ab3e6059ad93a96788076cac62c332f4d240b
MustafaMwafy <mwafy092@gmail.com>
2022-09-03 13:12:10 +0200

    add chart data column with mock data

--- src/components/ChartData.tsx
+++ src/components/ChartData.tsx
@@ -1,7 +1,45 @@
 import React, { FC } from 'react';
-
+import '../styles/chart-data.css';
 const ChartData: FC = () => {
-    return <div className='chart__data__container'></div>;
+    return (
+        <div className='chart__data__container'>
+            <div className='total__schools__data'>
+                <h2>
+                    <span className='total__lessons__num'>80</span> lessons
+                </h2>
+                <p>in kakuma</p>
+            </div>
+            <div className='schools__data__container'>
+                <div className='school__data'>
+                    <input type='radio' />
+                    <div className='school__info'>
+                        <h3>
+                            <span className='lesson__num'>10</span> lessons
+                        </h3>
+                        <p>in Greenlight</p>
+                    </div>
+                </div>
+                <div className='school__data'>
+                    <input type='radio' />
+                    <div className='school__info'>
+                        <h3>
+                            <span className='lesson__num'>10</span> lessons
+                        </h3>
+                        <p>in Greenlight</p>
+                    </div>
+                </div>
+                <div className='school__data'>
+                    <input type='radio' />
+                    <div className='school__info'>
+                        <h3>
+                            <span className='lesson__num'>10</span> lessons
+                        </h3>
+                        <p>in Greenlight</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export { ChartData };
