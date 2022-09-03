215e90dcd01de7faa5f7b87b51856983a92ed091
MustafaMwafy <mwafy092@gmail.com>
2022-09-03 02:50:00 +0200

    add chart with mock data

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,7 +1,39 @@
-import React, { FC } from 'react';
-
+import React, { FC, useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import 'chart.js/auto';
+import '../styles/chart.css';
 const Chart: FC = () => {
-    return <div className='chart__container'></div>;
+    const mockData = [
+        { id: 1, month: 'Jan', lessons: '100' },
+        { id: 2, month: 'Feb', lessons: '200' },
+        { id: 2, month: 'Mar', lessons: '50' },
+        { id: 2, month: 'Apr', lessons: '100' },
+        { id: 2, month: 'May', lessons: '150' },
+        { id: 2, month: 'Jun', lessons: '150' },
+        { id: 2, month: 'Jul', lessons: '150' },
+        { id: 2, month: 'Aug', lessons: '150' },
+        { id: 2, month: 'Sep', lessons: '150' },
+        { id: 2, month: 'Oct', lessons: '150' },
+        { id: 2, month: 'Nov', lessons: '0' },
+        { id: 2, month: 'Dec', lessons: '250' },
+    ];
+    const [chartData] = useState({
+        labels: mockData.map((m) => m.month),
+        datasets: [
+            {
+                label: 'lessons',
+                data: mockData.map((m) => m.lessons),
+            },
+        ],
+    });
+    return (
+        <div className='chart__container'>
+            <h3>No of lessons</h3>
+            <div className='chart'>
+                <Line data={chartData} />
+            </div>
+        </div>
+    );
 };
 
 export { Chart };
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react';
 import { Chart } from './Chart';
 import { ChartData } from './ChartData';
+import '../styles/dashboard.css';
 const Dashboard: FC = () => {
     return (
         <section className='dashboard__container'>
