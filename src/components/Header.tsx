58f3936831df7083fe60567ccad2cc93b8cfcdb3
MustafaMwafy <mwafy092@gmail.com>
2022-09-04 19:32:23 +0200

    add header tests

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,12 @@ import React, { FC } from 'react';
 import '../styles/header.css';
 const Header: FC = () => {
     return (
-        <header className='header__container'>
+        <header
+            className='header__container'
+            role='contentinfo'
+        >
             <h1>Analysis Chart</h1>
-            <h2>Number of sessons</h2>
+            <h2>Number of lessons</h2>
         </header>
     );
 };
