ec362d7543237c481e3b8132701d7a31e083ea87
MustafaMwafy <mwafy092@gmail.com>
2022-09-08 00:02:23 +0200

    redesign details page

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,35 +1,42 @@
-import React, { FC } from 'react';
-import { useLocation, Link } from 'react-router-dom';
-import '../styles/details.css';
+import React, { FC } from "react";
+import { useLocation, Link } from "react-router-dom";
+import "../styles/details.css";
+import backIcon from "../assets/back.png";
 const Details: FC = () => {
     const { state }: any = useLocation();
     return (
-        <div className='details__data__container'>
+        <div className="details__data__container">
             <Link
-                to='/'
-                role='link'
+                to="/"
+                role="link"
+                className="back__link"
             >
+                <img
+                    src={backIcon}
+                    width={20}
+                    alt="back icon"
+                />
                 Go Back To Home Page
             </Link>
-            <div className='details__data'>
+            <div className="details__data">
                 <h2>Country</h2>
                 <h3>{state?.country}</h3>
             </div>
-            <div className='details__data'>
+            <div className="details__data">
                 <h2>Camp</h2>
                 <h3>{state?.camp}</h3>
             </div>
-            <div className='details__data'>
+            <div className="details__data">
                 <h2>School</h2>
                 <h3>{state?.school}</h3>
             </div>
-            <div className='details__data'>
+            <div className="details__data">
                 <h2>Month</h2>
                 <h3>{state?.month}</h3>
             </div>
             <div
-                className='details__data'
-                role='detailsrole'
+                className="details__data"
+                role="detailsrole"
             >
                 <h2>Number of Lessons</h2>
                 <h3>{state?.lessons}</h3>
