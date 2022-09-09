65b3b050feba0561c86ed158bcb1dcc525bf5ddf
MustafaMwafy <mwafy092@gmail.com>
2022-09-09 15:15:21 +0200

    add filters tests

--- cypress/component/Filters.cy.tsx
+++ cypress/component/Filters.cy.tsx
@@ -2,6 +2,7 @@ import { Filters } from '../../src/components/Filters'
 import { Provider } from 'react-redux'
 import { store } from '../../src/store'
 import { BrowserRouter } from 'react-router-dom'
+import { getLessonsData, setLocationDataAction } from '../../src/reducers/lessons'
 
 describe('Test filters component', () => {
   it('Playground', () => {
@@ -12,5 +13,16 @@ describe('Test filters component', () => {
         </BrowserRouter>
       </Provider>,
     )
+    cy.window()
+      .its('store')
+      .then((state) => {
+        store.dispatch(getLessonsData())
+      })
+
+    // check for country list after dispatching
+    cy.get('#country').contains('Select Country')
+    cy.get('#Egypt').contains('Egypt')
+    cy.wait(2000)
+    cy.get('#country').select('Egypt')
   })
 })
